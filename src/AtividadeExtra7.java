b6f3a314a7525afd73864f0371b2898c22bfdfff
osvaldinho11 <joaovitorperico@gmail.com>
2024-09-19 14:40:38 -0300

    LaçoDeRepetição

---
+++ src/AtividadeExtra7.java
@@ -0,0 +1,11 @@
+public class AtividadeExtra7 {
+    public static void main(String[] args) {
+
+        int valor =0;
+
+        while (valor <= 100 ){
+            System.out.println(valor);
+            valor++;
+        }
+    }
+}
